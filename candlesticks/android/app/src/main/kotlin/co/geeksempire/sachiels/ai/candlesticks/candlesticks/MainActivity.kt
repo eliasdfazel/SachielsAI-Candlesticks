8eb1670af0b159c5caca366732cf3ad8cbd17b4a
Elias Fazel <elias.fazel@gmail.com>
2023-10-16 09:51:25 -0700

    Initializing Projects

--- candlesticks/android/app/src/main/kotlin/co/geeksempire/sachiels/ai/candlesticks/candlesticks/MainActivity.kt
+++ candlesticks/android/app/src/main/kotlin/co/geeksempire/sachiels/ai/candlesticks/candlesticks/MainActivity.kt
@@ -1,3 +1,13 @@
+/*
+ * Copyright © 2023 By Geeks Empire.
+ *
+ * Created by Elias Fazel
+ * Last modified 10/16/23, 9:51 AM
+ *
+ * Licensed Under MIT License.
+ * https://opensource.org/licenses/MIT
+ */
+
 package co.geeksempire.sachiels.ai.candlesticks.candlesticks
 
 import io.flutter.embedding.android.FlutterActivity
--- candlesticks/lib/main.dart
+++ candlesticks/lib/main.dart
@@ -1,3 +1,13 @@
+/*
+ * Copyright © 2023 By Geeks Empire.
+ *
+ * Created by Elias Fazel
+ * Last modified 10/16/23, 9:51 AM
+ *
+ * Licensed Under MIT License.
+ * https://opensource.org/licenses/MIT
+ */
+
 import 'package:flutter/material.dart';
 
 void main() {
