d73b6ac43ec5e91cde2dc9a298ea982d9245dc1e
Elias Fazel <elias.fazel@gmail.com>
2023-10-17 07:50:16 -0700

    [Description]
    [ChangeLog]
    ~ Firebase Added
    
    [Build][ChangeLog]
    # Libraries Updated

---
+++ lib/dashboard/ui/Dashboard.dart
@@ -0,0 +1,64 @@
+/*
+ * Copyright © 2023 By Geeks Empire.
+ *
+ * Created by Elias Fazel
+ * Last modified 10/17/23, 7:50 AM
+ *
+ * Licensed Under MIT License.
+ * https://opensource.org/licenses/MIT
+ */
+
+import 'package:candlesticks/resources/colors_resources.dart';
+import 'package:candlesticks/resources/strings_resources.dart';
+import 'package:candlesticks/utils/ui/system_bars.dart';
+import 'package:flutter/material.dart';
+
+class Dashboard extends StatefulWidget {
+
+  const Dashboard({Key? key}) : super(key: key);
+
+  @override
+  State<Dashboard> createState() => _DashboardState();
+}
+class _DashboardState extends State<Dashboard> {
+
+  @override
+  void initState() {
+    super.initState();
+
+    changeColor(ColorsResources.black, ColorsResources.black);
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return SafeArea(
+        child: MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: StringsResources.applicationName(),
+            color: ColorsResources.primaryColor,
+            theme: ThemeData(
+              fontFamily: 'Ubuntu',
+              colorScheme: ColorScheme.fromSwatch().copyWith(secondary: ColorsResources.primaryColor),
+              pageTransitionsTheme: const PageTransitionsTheme(builders: {
+                TargetPlatform.android: ZoomPageTransitionsBuilder(),
+                TargetPlatform.iOS: ZoomPageTransitionsBuilder(),
+              }),
+            ),
+            home: Scaffold(
+                resizeToAvoidBottomInset: false,
+                backgroundColor: ColorsResources.black,
+                body: Stack(
+                    children: [
+
+
+
+                    ]
+                )
+            )
+        )
+    );
+  }
+
+}
