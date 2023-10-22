0fcf46c15aed29cd4304cb9c902e4edec151f2a6
Elias Fazel <elias.fazel@gmail.com>
2023-10-22 07:44:12 -0700

    [Description]
    [ChangeLog]
    ~ Preview Decorated
    
    [Build][ChangeLog]
    # Libraries Updated

--- lib/previews/ui/PreviewInterface.dart
+++ lib/previews/ui/PreviewInterface.dart
@@ -2,36 +2,27 @@
  * Copyright © 2023 By Geeks Empire.
  *
  * Created by Elias Fazel
- * Last modified 10/19/23, 10:37 AM
+ * Last modified 10/22/23, 7:39 AM
  *
  * Licensed Under MIT License.
  * https://opensource.org/licenses/MIT
  */
 
-import 'package:candlesticks/dashboard/ui/sections/SachielsSignals.dart';
-import 'package:candlesticks/dashboard/ui/sections/account_information_overview.dart';
-import 'package:candlesticks/previews/ui/PreviewInterface.dart';
 import 'package:candlesticks/resources/colors_resources.dart';
 import 'package:candlesticks/resources/strings_resources.dart';
 import 'package:candlesticks/utils/modifications/numbers.dart';
-import 'package:candlesticks/utils/navigations/navigation_commands.dart';
 import 'package:candlesticks/utils/ui/display.dart';
 import 'package:candlesticks/utils/ui/system_bars.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_native_splash/flutter_native_splash.dart';
 
-class DashboardInterface extends StatefulWidget {
+class PreviewInterface extends StatefulWidget {
 
-  const DashboardInterface({Key? key}) : super(key: key);
+  PreviewInterface({Key? key}) : super(key: key);
 
   @override
-  State<DashboardInterface> createState() => _DashboardInterfaceState();
+  State<PreviewInterface> createState() => _PreviewInterfaceState();
 }
-class _DashboardInterfaceState extends State<DashboardInterface> {
-
-  AccountInformationOverview accountInformationOverview = const AccountInformationOverview();
-
-  ScrollController scrollController = ScrollController();
+class _PreviewInterfaceState extends State<PreviewInterface> {
 
   @override
   void initState() {
@@ -43,14 +34,6 @@ class _DashboardInterfaceState extends State<DashboardInterface> {
 
   @override
   Widget build(BuildContext context) {
-
-    Future.delayed(const Duration(milliseconds: 777), () {
-
-      FlutterNativeSplash.remove();
-
-    });
-
-    int gridColumnCount = (displayLogicalWidth(context) / 199).round();
 
     return SafeArea(
         child: MaterialApp(
@@ -244,159 +227,25 @@ class _DashboardInterfaceState extends State<DashboardInterface> {
                        * Start - List Background
                        */
                       Padding(
-                        padding: const EdgeInsets.fromLTRB(7, 137, 7, 7),
-                        child: Opacity(
-                          opacity: 0.73,
-                          child: ClipRRect(
-                            borderRadius: const BorderRadius.all(Radius.circular(17)),
-                            child: Image(
-                              image: const AssetImage("assets/roundangle_half.png"),
-                              width: displayLogicalWidth(context),
-                              fit: BoxFit.fill,
-                            )
+                          padding: const EdgeInsets.fromLTRB(7, 137, 7, 7),
+                          child: Opacity(
+                              opacity: 0.73,
+                              child: ClipRRect(
+                                  borderRadius: const BorderRadius.all(Radius.circular(17)),
+                                  child: Image(
+                                    image: const AssetImage("assets/roundangle_half.png"),
+                                    width: displayLogicalWidth(context),
+                                    fit: BoxFit.fill,
+                                  )
+                              )
                           )
-                        )
                       ),
                       /*
                        * End - List Background
                        */
                       /* End - Decorations */
 
-                      /*
-                       * Start - Content
-                       */
-                      /*
-                       * Start - Title
-                       */
-                      Padding(
-                          padding: const EdgeInsets.fromLTRB(25, 177, 25, 7),
-                          child: SizedBox(
-                            height: 59,
-                            width: displayLogicalWidth(context),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: [
 
-                                Expanded(
-                                  flex: 7,
-                                  child: SizedBox(
-                                    height: 59,
-                                    child: Align(
-                                      alignment: Alignment.centerLeft,
-                                      child: Text(
-                                        StringsResources.configuredCandlesticks(),
-                                        style: TextStyle(
-                                          color: ColorsResources.premiumLight,
-                                          fontSize: 23,
-                                          shadows: [
-                                            Shadow(
-                                              color: ColorsResources.primaryColorLighter.withOpacity(0.19),
-                                              blurRadius: 13,
-                                              offset: const Offset(-3, 3)
-                                            )
-                                          ]
-                                        ),
-                                      )
-                                    )
-                                  ),
-                                ),
-
-                                Expanded(
-                                  flex: 2,
-                                  child: SizedBox(
-                                    height: 59,
-                                    child: InkWell(
-                                      onTap: () {
-
-                                      },
-                                      child: Container(
-                                        padding: const EdgeInsets.fromLTRB(0, 7, 0, 0),
-                                        alignment: Alignment.centerRight,
-                                        child: const Image(
-                                          image: AssetImage("assets/search_icon.png"),
-                                          height: 43,
-                                        )
-                                      )
-                                    ),
-                                  ),
-                                )
-
-                              ]
-                            ),
-                          )
-                      ),
-                      /*
-                       * End - Title
-                       */
-
-                      /*
-                       * Start - List
-                       */
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(0, 237, 0, 7),
-                        child:GridView(
-                          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                            crossAxisCount: gridColumnCount,
-                            childAspectRatio: 0.61,
-                            mainAxisSpacing: 37.0,
-                            crossAxisSpacing: 19.0,
-                          ),
-                          padding: const EdgeInsets.fromLTRB(0, 0, 0, 37),
-                          physics: const NeverScrollableScrollPhysics(),
-                          scrollDirection: Axis.vertical,
-                          controller: scrollController,
-                          children: [
-
-                          ],
-                        )
-                      ),
-                      /*
-                       * End - List
-                       */
-                      /*
-                       * End - Content
-                       */
-
-                      /*
-                       * Start - Add
-                       */
-                      Positioned(
-                        bottom: 37,
-                        left: 19,
-                        right: 19,
-                        child: Center(
-                          child: SizedBox(
-                            height: 49,
-                            width: 239,
-                            child: InkWell(
-                              onTap: () {
-
-                                navigateTo(context, PreviewInterface());
-
-                              },
-                              child: const Image(
-                                image: AssetImage("assets/add_icon.png"),
-                              )
-                            )
-                          )
-                        )
-                      ),
-                      /*
-                       * Start - Add
-                       */
-
-                      /* Start - Account Information Overview */
-                      accountInformationOverview,
-                      /* End - Account Information Overview */
-
-                      /* Start - Purchase Plan Picker */
-                      const Positioned(
-                          right: 19,
-                          top: 19,
-                          child: SachielsSignals()
-                      ),
-                      /* End - Purchase Plan Picker */
 
                     ]
                 )
