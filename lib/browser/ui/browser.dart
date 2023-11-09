ad95974a314ee5e62e16a5f63bd1e37c06347829
Elias Fazel <elias.fazel@gmail.com>
2023-11-09 06:51:57 -0800

    [Description]
    [ChangeLog]
    ~ Search Functionalities Added
    
    [Build][ChangeLog]
    # Libraries Updated

--- lib/browser/ui/browser.dart
+++ lib/browser/ui/browser.dart
@@ -18,7 +18,6 @@ import 'package:candlesticks/utils/ui/system_bars.dart';
 import 'package:flutter/material.dart';
 import 'package:loading_animation_widget/loading_animation_widget.dart';
 import 'package:webview_flutter/webview_flutter.dart';
-import 'package:widget_mask/widget_mask.dart';
 
 class Browser extends StatefulWidget {
 
@@ -180,6 +179,10 @@ class _BrowserState extends State<Browser> {
                         });
 
                       },
+                      onWebViewCreated: (_) {
+                        debugPrint("Website Loaded Completely.");
+
+                      },
                     )
                 ),
                 /* End - Browser */
@@ -200,111 +203,6 @@ class _BrowserState extends State<Browser> {
                       )
                   )
                 ),
-
-                /* Start - Back */
-                Row(
-                  children: [
-
-                    /* Start - Back */
-                    Align(
-                        alignment: Alignment.topLeft,
-                        child: Padding(
-                            padding: const EdgeInsets.fromLTRB(19, 19, 0, 0),
-                            child: SizedBox(
-                                height: 59,
-                                width: 59,
-                                child: InkWell(
-                                  onTap: () {
-
-                                    navigatePop(context);
-
-                                  },
-                                  child: const Image(
-                                    image: AssetImage("assets/back_icon.png"),
-                                  ),
-                                )
-                            )
-                        )
-                    ),
-                    /* End - Back */
-
-                    /* Start - Title */
-                    Align(
-                        alignment: Alignment.topLeft,
-                        child: Padding(
-                            padding: const EdgeInsets.fromLTRB(19, 19, 0, 0),
-                            child: SizedBox(
-                                height: 59,
-                                width: 155,
-                                child: Stack(
-                                  children: [
-                                    WidgetMask(
-                                      blendMode: BlendMode.srcATop,
-                                      childSaveLayer: true,
-                                      mask /* Original Image */: Container(
-                                        decoration: const BoxDecoration(
-                                            gradient: LinearGradient(
-                                                colors: [
-                                                  ColorsResources.premiumDark,
-                                                  ColorsResources.black,
-                                                ],
-                                                transform: GradientRotation(45)
-                                            )
-                                        ),
-                                      ),
-                                      child: const Image(
-                                        image: AssetImage("assets/rectircle_shape.png"),
-                                      ),
-                                    ),
-                                    Align(
-                                        alignment: Alignment.center,
-                                        child: Padding(
-                                            padding: const EdgeInsets.all(1.9),
-                                            child: WidgetMask(
-                                                blendMode: BlendMode.srcATop,
-                                                childSaveLayer: true,
-                                                mask /* Original Image */: Container(
-                                                  decoration: const BoxDecoration(
-                                                      gradient: LinearGradient(
-                                                          colors: [
-                                                            ColorsResources.black,
-                                                            ColorsResources.premiumDark,
-                                                          ],
-                                                          transform: GradientRotation(45)
-                                                      )
-                                                  ),
-                                                ),
-                                                child: const Image(
-                                                  image: AssetImage("assets/rectircle_shape.png"),
-                                                )
-                                            )
-                                        )
-                                    ),
-                                    Align(
-                                        alignment: Alignment.centerLeft,
-                                        child: Padding(
-                                            padding: const EdgeInsets.fromLTRB(13, 0, 13, 0),
-                                            child: Text(
-                                                StringsResources.history(),
-                                                maxLines: 1,
-                                                overflow: TextOverflow.ellipsis,
-                                                style: const TextStyle(
-                                                    color: ColorsResources.premiumLight,
-                                                    fontSize: 19
-                                                )
-                                            )
-                                        )
-                                    )
-                                  ],
-                                )
-                            )
-                        )
-                    ),
-                    /* End - Title */
-
-                  ],
-                ),
-                /* End - Back */
 
               ],
             )
